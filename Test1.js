20f190d5727a16b55515fd246ad945c23d9a42b1
likoo-wool <86556261+likoo-wool@users.noreply.github.com>
2021-11-15 20:59:17 +0800

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -17,62 +17,46 @@ const $ = Env(jobname)
 
 async function all() {
 
-    await TimePacket();
+    await Rvideo();
     await $.wait(1000); 
   
 }
-/*
-function safeGet(data) {
-  try {
-    if (typeof JSON.parse(data) == "object") {
-      return true;
-    }
-  } catch (e) {
-    console.log(e);
-    console.log(`服务器访问数据为空，请检查自身设备网络情况`);
-    return false;
-  }
-}
+function Rvideo() {
+    return new Promise((resolve, reject) => {
+        const url = "https://ant.xunsl.com/v17/Rvideo/videoCallback.json";
+        const headers = {
 
-//首页视频奖励
-function sysp() {
-  return {
-    url: `https://ant.xunsl.com/v17/Rvideo/videoCallback.json`,
-    headers: {
+        };
+        const body = "access=4G&action=right_corner_video&app-version=8.3.2&app_name=jckd_app&app_version=8.3.2&carrier=CHN-CT&channel=c1005&device_brand=SMARTISAN&device_id=52000271&device_model=SM919&device_platform=android&device_type=android&dpi=560&inner_version=202109031457&language=zh-CN&memory=5&mi=0&mobile_type=1&net_type=2&network_type=4G&openudid=6f9c21802e9e7d69&os_api=23&os_version=MXB48T%20release-keys&request_time=1634452196&resolution=1440x2560&rom_version=MXB48T%20release-keys&s_ad=C7jvhIkVyrvg%3D-yk38pmM3JKhxvg4BmiQt9uAQgo9HJJXY&s_im=vCWzFwAroTSg%3Dm5oHIteOrCfqCdfjBg8NkQ%3D%3DU5&sim=1&sm_device_id=202109291605277cd2e35c7911bcbb3f30a0fecc28a12b01b20b831a219744&storage=52.62&subv=1.2.2&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJhY2Nlc3MiOiI0RyIsImFjdGlvbiI6InJpZ2h0X2Nvcm5lcl92aWRlbyIsImFwcC12ZXJzaW9uIjoiOC4zLjIiLCJhcHBfbmFtZSI6Impja2RfYXBwIiwiYXBwX3ZlcnNpb24iOiI4LjMuMiIsImNhcnJpZXIiOiJDSE4tQ1QiLCJjaGFubmVsIjoiYzEwMDUiLCJkZXZpY2VfYnJhbmQiOiJTTUFSVElTQU4iLCJkZXZpY2VfaWQiOiI1MjAwMDI3MSIsImRldmljZV9tb2RlbCI6IlNNOTE5IiwiZGV2aWNlX3BsYXRmb3JtIjoiYW5kcm9pZCIsImRldmljZV90eXBlIjoiYW5kcm9pZCIsImRwaSI6IjU2MCIsImlubmVyX3ZlcnNpb24iOiIyMDIxMDkwMzE0NTciLCJsYW5ndWFnZSI6InpoLUNOIiwibWVtb3J5IjoiNSIsIm1pIjoiMCIsIm1vYmlsZV90eXBlIjoiMSIsIm5ldF90eXBlIjoiMiIsIm5ldHdvcmtfdHlwZSI6IjRHIiwib3BlbnVkaWQiOiI2ZjljMjE4MDJlOWU3ZDY5Iiwib3NfYXBpIjoiMjMiLCJvc192ZXJzaW9uIjoiTVhCNDhUK3JlbGVhc2Uta2V5cyIsInJlcXVlc3RfdGltZSI6IjE2MzQ0NTIxOTYiLCJyZXNvbHV0aW9uIjoiMTQ0MHgyNTYwIiwicm9tX3ZlcnNpb24iOiJNWEI0OFQrcmVsZWFzZS1rZXlzIiwic19hZCI6IkM3anZoSWtWeXJ2ZyUzRC15azM4cG1NM0pLaHh2ZzRCbWlRdDl1QVFnbzlISkpYWSIsInNfaW0iOiJ2Q1d6RndBcm9UU2clM0RtNW9ISXRlT3JDZnFDZGZqQmc4TmtRJTNEJTNEVTUiLCJzaW0iOiIxIiwic21fZGV2aWNlX2lkIjoiMjAyMTA5MjkxNjA1Mjc3Y2QyZTM1Yzc5MTFiY2JiM2YzMGEwZmVjYzI4YTEyYjAxYjIwYjgzMWEyMTk3NDQiLCJzdG9yYWdlIjoiNTIuNjIiLCJzdWJ2IjoiMS4yLjIiLCJ1aWQiOiI1NTI0MjAxNCIsInZlcnNpb25fY29kZSI6IjgzMiIsInpxa2V5IjoiTURBd01EQXdNREF3TUpDTXBOLXcwOVd0ZzUtQmIzNmVoNkNQcUh1YWxJZWpsNi1GcldLd3pYV3hoWHlwNExEUHlHbDlvbnFrajNacVlKYThZODk4bmFqV3NKdXBaTERkaFdtRmpIN2Vycm02YXBxR2NYWSIsInpxa2V5X2lkIjoiZWFiOWU0MmI4NjU0NTYwOGFmNzAzMGJlYjkxZWEwNjkifQ.puiVKRhVCi5AS1lV87E-bzbdfm6ZUFiWMng2rgCi4ErtSW4v3_zybyjLCoSryIhJZJns62pz-Wvb-OBmkEFdkw&uid=55242014&version_code=832&zqkey=MDAwMDAwMDAwMJCMpN-w09Wtg5-Bb36eh6CPqHualIejl6-FrWKwzXWxhXyp4LDPyGl9onqkj3ZqYJa8Y898najWsJupZLDdhWmFjH7errm6apqGcXY&zqkey_id=eab9e42b86545608af7030beb91ea069";
+        const request = {
+            url: url,
+            headers: headers,
+            body: body
+        };
 
-	},
-    body: `access=4G&action=right_corner_video&app-version=8.3.2&app_name=jckd_app&app_version=8.3.2&carrier=CHN-CT&channel=c1005&device_brand=SMARTISAN&device_id=52000271&device_model=SM919&device_platform=android&device_type=android&dpi=560&inner_version=202109031457&language=zh-CN&memory=5&mi=0&mobile_type=1&net_type=2&network_type=4G&openudid=6f9c21802e9e7d69&os_api=23&os_version=MXB48T%20release-keys&request_time=1634452196&resolution=1440x2560&rom_version=MXB48T%20release-keys&s_ad=C7jvhIkVyrvg%3D-yk38pmM3JKhxvg4BmiQt9uAQgo9HJJXY&s_im=vCWzFwAroTSg%3Dm5oHIteOrCfqCdfjBg8NkQ%3D%3DU5&sim=1&sm_device_id=202109291605277cd2e35c7911bcbb3f30a0fecc28a12b01b20b831a219744&storage=52.62&subv=1.2.2&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJhY2Nlc3MiOiI0RyIsImFjdGlvbiI6InJpZ2h0X2Nvcm5lcl92aWRlbyIsImFwcC12ZXJzaW9uIjoiOC4zLjIiLCJhcHBfbmFtZSI6Impja2RfYXBwIiwiYXBwX3ZlcnNpb24iOiI4LjMuMiIsImNhcnJpZXIiOiJDSE4tQ1QiLCJjaGFubmVsIjoiYzEwMDUiLCJkZXZpY2VfYnJhbmQiOiJTTUFSVElTQU4iLCJkZXZpY2VfaWQiOiI1MjAwMDI3MSIsImRldmljZV9tb2RlbCI6IlNNOTE5IiwiZGV2aWNlX3BsYXRmb3JtIjoiYW5kcm9pZCIsImRldmljZV90eXBlIjoiYW5kcm9pZCIsImRwaSI6IjU2MCIsImlubmVyX3ZlcnNpb24iOiIyMDIxMDkwMzE0NTciLCJsYW5ndWFnZSI6InpoLUNOIiwibWVtb3J5IjoiNSIsIm1pIjoiMCIsIm1vYmlsZV90eXBlIjoiMSIsIm5ldF90eXBlIjoiMiIsIm5ldHdvcmtfdHlwZSI6IjRHIiwib3BlbnVkaWQiOiI2ZjljMjE4MDJlOWU3ZDY5Iiwib3NfYXBpIjoiMjMiLCJvc192ZXJzaW9uIjoiTVhCNDhUK3JlbGVhc2Uta2V5cyIsInJlcXVlc3RfdGltZSI6IjE2MzQ0NTIxOTYiLCJyZXNvbHV0aW9uIjoiMTQ0MHgyNTYwIiwicm9tX3ZlcnNpb24iOiJNWEI0OFQrcmVsZWFzZS1rZXlzIiwic19hZCI6IkM3anZoSWtWeXJ2ZyUzRC15azM4cG1NM0pLaHh2ZzRCbWlRdDl1QVFnbzlISkpYWSIsInNfaW0iOiJ2Q1d6RndBcm9UU2clM0RtNW9ISXRlT3JDZnFDZGZqQmc4TmtRJTNEJTNEVTUiLCJzaW0iOiIxIiwic21fZGV2aWNlX2lkIjoiMjAyMTA5MjkxNjA1Mjc3Y2QyZTM1Yzc5MTFiY2JiM2YzMGEwZmVjYzI4YTEyYjAxYjIwYjgzMWEyMTk3NDQiLCJzdG9yYWdlIjoiNTIuNjIiLCJzdWJ2IjoiMS4yLjIiLCJ1aWQiOiI1NTI0MjAxNCIsInZlcnNpb25fY29kZSI6IjgzMiIsInpxa2V5IjoiTURBd01EQXdNREF3TUpDTXBOLXcwOVd0ZzUtQmIzNmVoNkNQcUh1YWxJZWpsNi1GcldLd3pYV3hoWHlwNExEUHlHbDlvbnFrajNacVlKYThZODk4bmFqV3NKdXBaTERkaFdtRmpIN2Vycm02YXBxR2NYWSIsInpxa2V5X2lkIjoiZWFiOWU0MmI4NjU0NTYwOGFmNzAzMGJlYjkxZWEwNjkifQ.puiVKRhVCi5AS1lV87E-bzbdfm6ZUFiWMng2rgCi4ErtSW4v3_zybyjLCoSryIhJZJns62pz-Wvb-OBmkEFdkw&uid=55242014&version_code=832&zqkey=MDAwMDAwMDAwMJCMpN-w09Wtg5-Bb36eh6CPqHualIejl6-FrWKwzXWxhXyp4LDPyGl9onqkj3ZqYJa8Y898najWsJupZLDdhWmFjH7errm6apqGcXY&zqkey_id=eab9e42b86545608af7030beb91ea069`
-  }
-}
-
-function do_sysp() {
-  return new Promise(resolve => {
-    $.post(sysp(), (err, resp, data) => {
-      try {
-        if (err) {
-          console.log(`${JSON.stringify(err)}`)
-          console.log(resp)
-          console.log(`${$.name} API请求失败，请检查网路重试`)
-        } else {
-          if (safeGet(data)) {
-            const result = JSON.parse(data);
-			//console.log(`时段红包结果：${JSON.stringify(result)}\n`)
-			if (result.success == true) {
-				console.log(`\n首页时段激励奖励获取成功，获得 ${result.items.dialog.score} 金币！`)
-			} else {
-				console.log(`\n首页时段激励奖励获取失败:${JSON.stringify(result)}`)
-			}
-          }
-        }
-      } catch (e) {
-        $.logErr(e, resp)
-      } finally {
-        resolve();
-      }
+        $.post(request, async (error, response, data) => {
+            try {
+                //$.log(data);
+                const result=JSON.parse(data);           
+                if(result.items&&result.items.dialog&&result.items.dialog.score)
+                {
+                    $.log(`【视频奖励】+${result.items.dialog.score}金币`);
+                    await S.wait(1000);
+                    await Rvideo();
+                }
+                else
+                {
+                    $.log(`【视频奖励】${result.message}`);
+                }
+                    
+            } catch (e) {
+                $.log(e)
+            }
+            resolve();
+        })
     })
-  })
 }
-*/
+/*
 //时段奖励
 function TimePacket() {
     return new Promise((resolve, reject) => {
@@ -107,7 +91,7 @@ function TimePacket() {
         })
     })
 }
-
+*/
 
 function Env(t, e) {
     class s {
